d17628dae9a8e3e7b214b1582348de72c79a708c
Luis Hernandez <luis@maniak.com.mx>
2018-02-16 21:02:56 -0500

    Cleans up stuff

--- build/webpack.server.js
+++ build/webpack.server.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const webpack = require("webpack");
 const merge = require("webpack-merge");
 const nodeExternals = require("webpack-node-externals");
 const VueSSRServerPlugin = require("vue-server-renderer/server-plugin");
